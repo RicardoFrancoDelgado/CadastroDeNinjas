e4b66fea04d6fb470cb814d1d093e68e424e8a77
RicardoFrancoDelgado <rf041555@gmail.com>
2026-05-07 01:50:53 -0300

    fetatÃÃ: Cria mock de CRUD de ninjas

--- src/main/java/dev/ricardo/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/ricardo/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,7 +1,6 @@
 package dev.ricardo.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class NinjaController {
@@ -11,5 +10,32 @@ public class NinjaController {
         return "Essa é a mensagem de boas vindas";
     }
 
+    // Adicionar ninja - CREATE
+    @PostMapping("/criar")
+    public String criarNinja() {
+        return "Ninja criado";
+    }
 
+    // Mostrar ninja por id - READ
+    @GetMapping("/mostrar/:id")
+    public String MostrarTodosOsNinjasID() {
+        return "Mostrar todos por id";
+    }
+
+    // Mostrar todos o ninjas - READ
+    @GetMapping("/mostrar")
+    public String MostrarTodosOsNinjas() {
+        return "Mostrar todos";
+    }
+    // Alterar dados dos ninjas - UPDATE
+    @PutMapping("/alterar")
+    public String AlterarNinja() {
+        return "Alterar ninja";
+    }
+
+    // Deletar ninja - DELETE
+    @DeleteMapping("/deletar/:id")
+    public String DeletarNinjaPorID() {
+        return "Deletar";
+    }
 }
