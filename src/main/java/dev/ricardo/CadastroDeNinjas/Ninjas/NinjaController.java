49bfb1fcd2a9ecc8bbec3c046e6feca77b2331f0
RicardoFrancoDelgado <rf041555@gmail.com>
2026-06-08 12:37:04 -0300

    fix: Remove imports que não estão sendo mais usados

--- src/main/java/dev/ricardo/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/ricardo/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
